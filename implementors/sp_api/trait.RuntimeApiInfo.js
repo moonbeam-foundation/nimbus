f244e93acb2e73145c04728294f5fb4f62a078c0
JoshOrndorff <JoshOrndorff@users.noreply.github.com>
2021-11-01 19:00:48 +0000

    deploy: 9376611469cd6b3377be0896ed61b0a5a6b45696

--- implementors/sp_api/trait.RuntimeApiInfo.js
+++ implementors/sp_api/trait.RuntimeApiInfo.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["nimbus_primitives"] = [{"text":"impl&lt;Block:&nbsp;BlockT, AuthorId:&nbsp;Codec&gt; RuntimeApiInfo for dyn <a class=\"trait\" href=\"nimbus_primitives/trait.AuthorFilterAPI.html\" title=\"trait nimbus_primitives::AuthorFilterAPI\">AuthorFilterAPI</a>&lt;Block, AuthorId&gt;","synthetic":false,"types":[]}];
+implementors["nimbus_primitives"] = [{"text":"impl&lt;Block:&nbsp;BlockT, AuthorId:&nbsp;Codec&gt; RuntimeApiInfo for dyn <a class=\"trait\" href=\"nimbus_primitives/trait.NimbusApi.html\" title=\"trait nimbus_primitives::NimbusApi\">NimbusApi</a>&lt;Block, AuthorId&gt;","synthetic":false,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
