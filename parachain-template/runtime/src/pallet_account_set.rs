bda8f444fb2efd9ccf8537e9dbe8e9ece8b6e37c
Joshy Orndorff <admin@joshyorndorff.com>
2021-12-02 10:19:47 -0500

    template is unlicensed

--- parachain-template/runtime/src/pallet_account_set.rs
+++ parachain-template/runtime/src/pallet_account_set.rs
@@ -1,19 +1,3 @@
-// Copyright 2019-2021 PureStake Inc.
-// This file is part of Nimbus.
-
-// Nimbus is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-
-// Nimbus is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-
-// You should have received a copy of the GNU General Public License
-// along with Nimbus.  If not, see <http://www.gnu.org/licenses/>.
-
 //! Small pallet responsible for storing a set of accounts, and their associated session keys.
 //! This is a minimal solution where staking would be used in practice.
 //! The accounts are set and genesis and never change.
