8fb46276bdb6f3ed4e7f35c02495c13d3ed41dc7
librelois <c@elo.tf>
2021-12-02 15:26:44 +0100

    replace Moonbeam by Nimbus

--- pallets/aura-style-filter/src/lib.rs
+++ pallets/aura-style-filter/src/lib.rs
@@ -1,18 +1,18 @@
 // Copyright 2019-2021 PureStake Inc.
-// This file is part of Moonbeam.
+// This file is part of Nimbus.
 
-// Moonbeam is free software: you can redistribute it and/or modify
+// Nimbus is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 
-// Moonbeam is distributed in the hope that it will be useful,
+// Nimbus is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
 
 // You should have received a copy of the GNU General Public License
-// along with Moonbeam.  If not, see <http://www.gnu.org/licenses/>.
+// along with Nimbus.  If not, see <http://www.gnu.org/licenses/>.
 
 //! A Nimbus filter for the AuRa consensus algorithm. This filter does not use any entropy, it
 //! simply rotates authors in order. A single author is eligible at each slot.
