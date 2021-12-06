9f65a4522e52a6ada4e4a30de2908c9a3debb37c
Joshy Orndorff <admin@joshyorndorff.com>
2021-12-06 15:48:30 -0500

    Typo found by @girazoki

--- nimbus-consensus/src/lib.rs
+++ nimbus-consensus/src/lib.rs
@@ -231,7 +231,7 @@ where
 	};
 
 	// Iterate keys until we find an eligible one, or run out of candidates.
-	// If we are skipping prediction, then we author withthe first key we find.
+	// If we are skipping prediction, then we author with the first key we find.
 	// prediction skipping only really amkes sense when there is a single key in the keystore.
 	let maybe_key = available_keys.into_iter().find(|type_public_pair| {
 		// Have to convert to a typed NimbusId to pass to the runtime API. Maybe this is a clue
