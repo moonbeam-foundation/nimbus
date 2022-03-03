6571bb414fd5e06ed28272e5cacd9e34819112ef
gorka <gorka.irazoki@gmail.com>
2022-03-03 15:16:19 +0100

    Add tests and benchmarks for set_eligibility in author-slot-filter

--- pallets/author-slot-filter/src/lib.rs
+++ pallets/author-slot-filter/src/lib.rs
@@ -29,6 +29,9 @@ use frame_support::pallet;
 
 pub use pallet::*;
 
+#[cfg(any(test, feature = "runtime-benchmarks"))]
+mod benchmarks;
+
 #[pallet]
 pub mod pallet {
 
@@ -134,6 +137,7 @@ pub mod pallet {
 
 	/// The percentage of active authors that will be eligible at each height.
 	#[pallet::storage]
+	#[pallet::getter(fn eligible_ratio)]
 	pub type EligibleRatio<T: Config> = StorageValue<_, Percent, ValueQuery, Half<T>>;
 
 	// Default value for the `EligibleRatio` is one half.
@@ -168,5 +172,92 @@ pub mod pallet {
 	pub enum Event {
 		/// The amount of eligible authors for the filter to select has been changed.
 		EligibleUpdated(Percent),
+	}
+}
+
+#[cfg(test)]
+pub mod tests {
+	use super::*;
+	use crate as author_slot_filter;
+
+	use frame_support::{assert_ok, parameter_types, traits::Everything};
+	use frame_support_test::TestRandomness;
+	use sp_core::H256;
+	use sp_io::TestExternalities;
+	use sp_runtime::{
+		testing::Header,
+		traits::{BlakeTwo256, IdentityLookup},
+		Percent,
+	};
+	use sp_std::vec;
+	const AUTHOR_ID: u64 = 1;
+
+	pub fn new_test_ext() -> TestExternalities {
+		let t = frame_system::GenesisConfig::default()
+			.build_storage::<Test>()
+			.unwrap();
+		TestExternalities::new(t)
+	}
+
+	type UncheckedExtrinsic = frame_system::mocking::MockUncheckedExtrinsic<Test>;
+	type Block = frame_system::mocking::MockBlock<Test>;
+
+	// Configure a mock runtime to test the pallet.
+	frame_support::construct_runtime!(
+		pub enum Test where
+			Block = Block,
+			NodeBlock = Block,
+			UncheckedExtrinsic = UncheckedExtrinsic,
+		{
+			System: frame_system::{Pallet, Call, Config, Storage, Event<T>},
+			AuthorSlotFilter: author_slot_filter::{Pallet, Call, Storage, Event, Config},
+		}
+	);
+
+	parameter_types! {
+		pub const BlockHashCount: u64 = 250;
+	}
+	impl frame_system::Config for Test {
+		type BaseCallFilter = Everything;
+		type BlockWeights = ();
+		type BlockLength = ();
+		type DbWeight = ();
+		type Origin = Origin;
+		type Index = u64;
+		type BlockNumber = u64;
+		type Call = Call;
+		type Hash = H256;
+		type Hashing = BlakeTwo256;
+		type AccountId = u64;
+		type Lookup = IdentityLookup<Self::AccountId>;
+		type Header = Header;
+		type Event = Event;
+		type BlockHashCount = BlockHashCount;
+		type Version = ();
+		type PalletInfo = PalletInfo;
+		type AccountData = ();
+		type OnNewAccount = ();
+		type OnKilledAccount = ();
+		type SystemWeightInfo = ();
+		type SS58Prefix = ();
+		type OnSetCode = ();
+	}
+	parameter_types! {
+		pub Authors: Vec<u64> = vec![AUTHOR_ID];
+	}
+	impl Config for Test {
+		type Event = Event;
+		type RandomnessSource = TestRandomness<Self>;
+		type PotentialAuthors = Authors;
+	}
+
+	#[test]
+	fn set_eligibility_works() {
+		new_test_ext().execute_with(|| {
+			let percent = Percent::from_percent(34);
+
+			assert_ok!(AuthorSlotFilter::set_eligible(Origin::root(), percent));
+			assert_eq!(AuthorSlotFilter::eligible_ratio(), percent)
+		});
 	}
 }
