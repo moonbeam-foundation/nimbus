f3b7a0125d52dc51e358bf4b24dbbb4adc288a41
girazoki <girazoki@users.noreply.github.com>
2022-03-16 17:02:18 +0000

    deploy: 34b074f82121e48df63c0122e4c7609c9dcf6192

---
+++ implementors/pallet_author_slot_filter/weights/trait.WeightInfo.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["pallet_author_slot_filter"] = [];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
