2a0432d32f7856397c9a213f1be185bf1f052fef
nbaztec <nbaztec@users.noreply.github.com>
2022-03-29 15:04:53 +0000

    deploy: 7b6bc6065178256e71cc8a1e9007f64878144d9c

---
+++ implementors/core/hash/trait.Hash.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["pallet_author_slot_filter"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"struct\" href=\"pallet_author_slot_filter/num/struct.NonZeroU32.html\" title=\"struct pallet_author_slot_filter::num::NonZeroU32\">NonZeroU32</a>","synthetic":false,"types":["pallet_author_slot_filter::num::NonZeroU32"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
