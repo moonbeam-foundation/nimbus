a10e603f650b89ac9ab7da31e9eeae34ccdc5eff
4meta5 <asinghchrony@protonmail.com>
2022-04-07 13:59:43 -0400

    fix weight estimate

--- pallets/author-inherent/src/lib.rs
+++ pallets/author-inherent/src/lib.rs
@@ -117,7 +117,7 @@ pub mod pallet {
 		/// but before transactions are executed.
 		// This should go into on_post_inherents when it is ready https://github.com/paritytech/substrate/pull/10128
 		// TODO better weight. For now we just set a somewhat conservative fudge factor
-		#[pallet::weight((10 * T::DbWeight::get().write, DispatchClass::Mandatory))]
+		#[pallet::weight((3 * T::DbWeight::get().read + 11 * T::DbWeight::get().write, DispatchClass::Mandatory))]
 		pub fn kick_off_authorship_validation(origin: OriginFor<T>) -> DispatchResultWithPostInfo {
 			ensure_none(origin)?;
 
