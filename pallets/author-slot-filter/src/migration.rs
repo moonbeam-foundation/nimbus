e01952f8358b869fbc86d2cbfa5805b448c03e39
Nisheeth Barthwal <nbaztec@gmail.com>
2022-04-28 16:30:47 +0200

    use pallet storages directly

--- pallets/author-slot-filter/src/migration.rs
+++ pallets/author-slot-filter/src/migration.rs
@@ -15,7 +15,6 @@
 // along with Nimbus.  If not, see <http://www.gnu.org/licenses/>.
 
 use core::marker::PhantomData;
-use frame_support::storage::migration;
 use frame_support::traits::Get;
 use frame_support::traits::OnRuntimeUpgrade;
 use frame_support::weights::Weight;
@@ -27,12 +26,10 @@ use frame_support::traits::OnRuntimeUpgradeHelpersExt;
 use super::num::NonZeroU32;
 use super::pallet::Config;
 use super::pallet::EligibilityValue;
+use super::pallet::EligibleCount;
+use super::pallet::Pallet;
 
 pub struct EligibleRatioToEligiblityCount<T>(PhantomData<T>);
-
-pub const PALLET_NAME: &[u8] = b"AuthorFilter";
-pub const ELIGIBLE_RATIO_ITEM_NAME: &[u8] = b"EligibleRatio";
-pub const ELIGIBLE_COUNT_ITEM_NAME: &[u8] = b"EligibleCount";
 
 impl<T> OnRuntimeUpgrade for EligibleRatioToEligiblityCount<T>
 where
@@ -41,34 +38,23 @@ where
 	fn on_runtime_upgrade() -> Weight {
 		log::info!(target: "EligibleRatioToEligiblityCount", "starting migration");
 
-		let old_value =
-			migration::get_storage_value::<Percent>(PALLET_NAME, ELIGIBLE_RATIO_ITEM_NAME, &[]);
+		let old_value = <Pallet<T>>::eligible_ratio();
+		let total_authors = <T as Config>::PotentialAuthors::get().len();
+		let new_value = percent_of_num(old_value, total_authors as u32);
+		let new_value = NonZeroU32::new(new_value).unwrap_or(EligibilityValue::default());
 
-		let new_value = old_value
-			.and_then(|value| {
-				let total_authors = <T as Config>::PotentialAuthors::get().len();
-				let new_value = percent_of_num(value, total_authors as u32);
-				NonZeroU32::new(new_value)
-			})
-			.unwrap_or(EligibilityValue::default());
+		<EligibleCount<T>>::put(new_value);
 
-		let db_weights = T::DbWeight::get();
-		migration::put_storage_value(PALLET_NAME, ELIGIBLE_COUNT_ITEM_NAME, &[], new_value);
-		db_weights.write + db_weights.read
+		T::DbWeight::get().reads_writes(1, 1)
 	}
 
 	#[cfg(feature = "try-runtime")]
 	fn pre_upgrade() -> Result<(), &'static str> {
-		let old_value =
-			migration::get_storage_value::<Percent>(PALLET_NAME, ELIGIBLE_RATIO_ITEM_NAME, &[]);
+		let old_value = <Pallet<T>>::eligible_ratio();
 
-		let expected_value = old_value
-			.and_then(|value| {
-				let total_authors = <T as Config>::PotentialAuthors::get().len();
-				let eligible_count: u32 = percent_of_num(value, total_authors as u32);
-				NonZeroU32::new(eligible_count)
-			})
-			.unwrap_or(EligibilityValue::default());
+		let total_authors = <T as Config>::PotentialAuthors::get().len();
+		let new_value = percent_of_num(old_value, total_authors as u32);
+		let expected_value = NonZeroU32::new(new_value).unwrap_or(EligibilityValue::default());
 
 		Self::set_temp_storage(expected_value, "expected_eligible_count");
 
@@ -78,8 +64,7 @@ where
 	#[cfg(feature = "try-runtime")]
 	fn post_upgrade() -> Result<(), &'static str> {
 		let expected = Self::get_temp_storage::<NonZeroU32>("expected_eligible_count");
-		let actual =
-			migration::get_storage_value::<NonZeroU32>(PALLET_NAME, ELIGIBLE_COUNT_ITEM_NAME, &[]);
+		let actual = <Pallet<T>>::eligible_count();
 
 		assert_eq!(expected, actual);
 
@@ -105,5 +90,16 @@ mod tests {
 
 		let actual = percent_of_num(fifty_percent, 20);
 		assert_eq!(10, actual);
+	}
+
+	#[test]
+	fn test_percent_of_num_hundred_percent_uses_full_value() {
+		let one_hundred_percent = Percent::from_float(1.0);
+
+		let actual = percent_of_num(one_hundred_percent, 5);
+		assert_eq!(5, actual);
+
+		let actual = percent_of_num(one_hundred_percent, 20);
+		assert_eq!(20, actual);
 	}
 }
