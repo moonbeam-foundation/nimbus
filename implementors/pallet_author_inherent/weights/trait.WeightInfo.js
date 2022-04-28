347cf270ee868901152a75cee2cec4bd67767cfd
girazoki <girazoki@users.noreply.github.com>
2022-04-28 09:22:36 +0000

    deploy: 2839edbd2789de33cd4e145560c3aa0b31f5529d

---
+++ implementors/pallet_author_inherent/weights/trait.WeightInfo.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["pallet_author_inherent"] = [];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
