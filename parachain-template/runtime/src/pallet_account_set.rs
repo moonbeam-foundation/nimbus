13ed96e219cc0b10ff0daf361da64e5b808eb628
tgmichel <telmo@purestake.com>
2023-03-01 21:33:33 +0100

    Fix links

--- parachain-template/runtime/src/pallet_account_set.rs
+++ parachain-template/runtime/src/pallet_account_set.rs
@@ -6,7 +6,7 @@
 //! pallet could be replaced with.
 //! Gautam's validator set pallet - https://github.com/paritytech/substrate/tree/master/frame/staking/
 //! Parity's pallet staking - https://github.com/paritytech/substrate/tree/master/frame/staking/
-//! Moonbeam's Parachain Staking - https://github.com/paritytech/moonbeam/tree/master/pallets/parachain-staking
+//! Moonbeam's Parachain Staking - https://github.com/PureStake/moonbeam/tree/master/pallets/parachain-staking
 //! Recipe for AccountSet, VecSet, and MapSet
 
 use frame_support::pallet;
